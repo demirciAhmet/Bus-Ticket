e4266bcdc390ec0828c34502a383709e4a2f5ebd
terrabladex <ahmetkaandemirci@outlook.com>
2024-01-26 20:32:35 +0300

    update

---
+++ SingleSeatBus.java
@@ -0,0 +1,14 @@
+
+public class SingleSeatBus extends Bus{
+	
+	
+	public SingleSeatBus(String busType, String plateNumber, int numberOfRows,int posOfMidDoor) {
+		super(busType, plateNumber, numberOfRows, posOfMidDoor);
+		seatPerRow=3;
+		corridorPos=2;
+		createSeatLayout();
+	}
+
+
+	
+}
